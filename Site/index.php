1cd639712b82eae113c886245f8cbc9cb20f23ea
tiagfernandes <tiago_fernandes@live.fr>
2015-05-19 14:43:21 +0200

    sorry

--- Site/index.php
+++ Site/index.php
@@ -11,9 +11,14 @@
 <?php include_once('connexion.php'); ?>
       
     <body>
-        <div id="contenu">
-           
 
+        <div>
+        <?php
+             $req1 = "SELECT * FROM utilisateur;";
+             $result = mysql_query($req1) or die (mysql_error());
+             $ligne=mysql_fetch_array($result1);
+             echo "<p>Nom utilisateur : ".$ligne[0]."</p>";
+        ?><p>Bonjour</p>
         </div>
     </body>
 </html>
