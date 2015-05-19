02ce82b816bb59821999b3b3ef3afbc1491d5c41
tiagfernandes <tiago_fernandes@live.fr>
2015-05-19 10:40:55 +0200

    ss

--- Site/index.php
+++ Site/index.php
@@ -14,3 +14,30 @@
         </div>
     </body>
 </html>
+<?php // Connexion à la base de donnée + SESSION.
+session_start();
+
+// Configuration.
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "augustine";
+
+$pdo = null;
+
+$DEV = true;
+
+try {
+    $connStr = 'mysql:host='.$host.';dbname='.$db;
+    $arrExtraParam= array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+    $pdo = new PDO($connStr, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec('SET NAMES utf8');
+} catch(PDOException $e) {
+    if($DEV) :
+      $msg = 'ERREUR PDO dans ' . $e->getFile() . ' L.' . $e->getLine() . ' : ' . $e->getMessage();
+    else :
+      $msg = 'ERREUR de connexion à la base de données';
+    endif;
+    die($msg);
+}
