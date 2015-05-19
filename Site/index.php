f53feffd0a5d8b5e4874f508c3da94eacf6655bd
thomasplessiet <thomas.plessiet@laposte.net>
2015-05-19 10:12:42 +0200

    idenx

--- Site/index.php
+++ Site/index.php
@@ -1,8 +1,14 @@
 <!doctype html>
 <html lang="fr">
+
    <head>
     <title>Index</title>
     <link rel="shortcut icon" type="image/x-icon" href="./image/favicon.ico" />
     <link rel="icon" type="image/x-icon" href="./image/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="style.css">
     </head>
+
+    <body>
+
+    </body>
 </html>
