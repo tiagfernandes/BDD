821826532033f84f04632dc10005576e218a78eb
tiagfernandes <tiago_fernandes@live.fr>
2015-05-19 15:13:41 +0200

    t

--- Site/index.php
+++ Site/index.php
@@ -15,11 +15,6 @@
         <div class="">
         <?php
 
-            $req1 = 'SELECT * FROM utilisateur;';
-            $result = mysql_query($req1) or die (mysql_error());
-            $ligne=mysql_fetch_array($result1);
-            echo "<p>Nom utilisateur : ".$ligne[0]."</p>";
-
             $req1 = 'SELECT * FROM utilisateur';
             $result = $pdo->query(req1);
         while ($row = $result->fetch())
