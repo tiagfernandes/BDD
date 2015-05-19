7e012ac3b52a046dc6040e79611742f2af926e4c
tiagfernandes <tiago_fernandes@live.fr>
2015-05-19 15:12:46 +0200

    Zero

--- Site/index.php
+++ Site/index.php
@@ -12,32 +12,23 @@
       
     <body>
 
-       <div id="entete">
-
-       </div>
-
         <div class="">
         <?php
-<<<<<<< HEAD
-             $req1 = 'SELECT * FROM utilisateur;';
-             $result = mysql_query($req1) or die (mysql_error());
-             $ligne=mysql_fetch_array($result1);
-             echo "<p>Nom utilisateur : ".$ligne[0]."</p>";
-=======
+
+            $req1 = 'SELECT * FROM utilisateur;';
+            $result = mysql_query($req1) or die (mysql_error());
+            $ligne=mysql_fetch_array($result1);
+            echo "<p>Nom utilisateur : ".$ligne[0]."</p>";
+
             $req1 = 'SELECT * FROM utilisateur';
             $result = $pdo->query(req1);
         while ($row = $result->fetch())
         { ?>
 
         <p>Nom : <?php>print($row[]);?></p>
-<<<<<<< HEAD
-        }
-=======
->>>>>>> origin/master
-        ?>
 
-        <p>Bonjour</p>
->>>>>>> origin/master
+        }
+
         </div>
     </body>
 </html>
