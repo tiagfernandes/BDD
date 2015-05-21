22aa85c0389b26fb3bc9076de43962050aa1205a
thomasplessiet <thomas.plessiet@laposte.net>
2015-05-21 09:21:41 +0200

    log

--- Site/identification.php
+++ Site/identification.php
@@ -1,24 +1,30 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
+    header('Content-Type: text/html; charset=utf-8');
 
-$login_valide = "root";
-$pwd_valide = "sio";
+    $login_valide = "root";
+    $pwd_valide = "sio";
 
-if (isset($_POST['login']) && isset($_POST['pwd'])) {
-    if ($login_valide == $_POST['login'] && $pwd_valide == $_POST['pwd']) {
-        session_start ();
-        $_SESSION['login'] = $_POST['login'];
-		$_SESSION['pwd'] = $_POST['pwd'];
-        header ('location: page_membre.php');
+    if (empty($_POST['login']) && empty($_POST['pwd'])) {
+
+        echo '<body onLoad="alert(\'Entrez un champs.\')">';
+        echo '<meta http-equiv="refresh" content="0;URL=formulaire-co.php">';
+        header ('location: formulaire-co.php');
+
     }
 
     else {
-        echo '<body onLoad="alert(\'Membre non reconnu...\')">';
 
-		echo '<meta http-equiv="refresh" content="0;URL=formulaire-co.php">';
-    }
-}
-else {
-	echo 'Les variables du formulaire ne sont pas déclarées.';
-}
-?>
+        if ($login_valide == $_POST['login'] && $pwd_valide == $_POST['pwd']) {
+            session_start ();
+            $_SESSION['login'] = $_POST['login'];
+            $_SESSION['pwd'] = $_POST['pwd'];
+            header ('location: page_membre.php');
+        }
+
+        else
+            header ('location: formulaire-co.php');
+            echo '<body onLoad="alert(\'Identifiant ou mots de passe incorrect.\')">';
+
+        }
+
+    ?>
