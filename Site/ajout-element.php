93d90ff0116e607f1a683279543c16d20e9b2e02
tiagfernandes <tiago_fernandes@live.fr>
2015-05-26 15:29:02 +0200

    j

--- Site/ajout-element.php
+++ Site/ajout-element.php
@@ -21,7 +21,18 @@
     <?php require_once('entete.php'); ?>
 
         <div><h2>Ajout d'un équipement</h2>
+          <form method="post" action="">
 
+            <label>Nom équipement : </label><input type="text" name="nom_equipement" placeholder="Nom"></p>
+            <label>Etiquette : </label><input type="text" name="etiquette" placeholder="Etiquette"></p>
+            <label>Catégorie : </label></p>
+            <select name="Catégorie">
+                <option value=0>-- Catégorie --</option>
+                <option value="">Capteur</option>
+                <option value="">Moteur</option>
+             </select>
+            <input class="bouton" type="submit" value="Ajouter">
+          </form>
 
         </div>
     </body>
