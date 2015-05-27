3b65702c7fa06e78505792bf9aeccc196eb1eb4c
tiagfernandes <tiago_fernandes@live.fr>
2015-05-27 11:57:10 +0200

    de

--- Site/entete.php
+++ Site/entete.php
@@ -1,7 +1,7 @@
 <div id="entete">
         <script type="text/javascript" src="date_heure.js"></script>
        <?php require_once('information-utilisateur.php') ?>
-<<<<<<< HEAD
+
         <div id="onglet">
             <ul id="nav">
                 <li><a href="index.php">Accueil</a></li>
@@ -9,17 +9,15 @@
                 <li><a href="profil.php">Profil</a></li>
                 <li><a href="admin.php">Administration</a></li>
            </ul>
-=======
 
         <div class="deco">
             <a href="logout.php" onclick="return(confirm('Etes-vous sûr de vouloir vous déconnectez ? '));">Déconnexion</a>
->>>>>>> origin/master
         </div>
+
         <div id="time">
             <span id="date_heure"></span>
             <script type="text/javascript">window.onload = date_heure('date_heure');</script>
         </div>
-        <div class="deco">
-            <a href="logout.php" onclick="return(confirm('Etes-vous sûr de vouloir vous déconnectez ? '));">Déconnexion</a>
-        </div>
+
+
 </div>
