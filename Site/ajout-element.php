f77a64afcd94fb11d42429ce32b0b5e6527b5a83
tiagfernandes <tiago_fernandes@live.fr>
2015-05-27 14:59:10 +0200

    fsdf

--- Site/ajout-element.php
+++ Site/ajout-element.php
@@ -28,39 +28,50 @@
 
         <form method="post" action="ajout.php">
 
-            <label id="ajout_element">Nom équipement : </label><input type="text" name="nom_equipement" placeholder="Nom"></p>
+            <label id="ajout_element">Nom équipement : *</label><input type="text" name="nom_equipement" placeholder="Nom"></p>
 
-            <label id="ajout_element">Etiquette : </label></p>
+            <label id="ajout_element">Etiquette : *</label></p>
+
+
+                <!-- 1ere listview -->
                 <select name="plateforme">
-                    <option value=NULL>-- Plateforme --</option>
-                    <option value="Serre">Serre</option>
-                    <option value="Eco">Eco</option>
-                    <option value="Plan">Plan</option>
-                    <option value="ML">ML</option>
-                    <option value="Vol">Vol</option>
+                    <option value=NULL>-- Catégorie --</option>
+                    <?php
+                    $reponse = $pdo->query('SELECT * FROM categorie_etiquette');
+                    while ($donnees = $reponse->fetch()){
+                    ?>
+                        <option value="<?php echo $donnees['valeur_categorie']; ?>"><?php echo $donnees['valeur_categorie']; ?> - <?php echo $donnees['categorieEtiquette']; ?></option>
+                    <?php
+                    }
+                    ?>
                 </select> -
+
+
+                <!-- 2eme listview -->
                 <select name="piece">
-                    <option value=NULL>-- Pièce --</option>
-                    <option value="BC">BC</option>
-                    <option value="BE">BE</option>
-                    <option value="BS">BS</option>
-                    <option value="LC">LC</option>
-                    <option value="LCh">LCh</option>
-                    <option value="LH">LH</option>
-                    <option value="LM">LM</option>
-                    <option value="Lav">Lav</option>
-                    <option value="LL">LL</option>
-                    <option value="LP">LP</option>
+                   <option value=NULL>-- Acronime --</option>
+                    <?php
+                    $reponse = $pdo->query('SELECT * FROM acronime_etiquette');
+                    while ($donnees = $reponse->fetch()){
+                    ?>
+                        <option value="<?php echo $donnees['valeur_acronime']; ?>"><?php echo $donnees['valeur_acronime']; ?> - <?php echo $donnees['acronimeEtiquette']; ?></option>
+                    <?php
+                    }
+                    ?>
                  </select> -
-                <input type="text" name="emplacement" placeholder="Emplacement"></p>
+                <input type="text" name="nEquipement" placeholder="Numéro équiepement"></p>
+
+
+
             <label id="ajout_element">Prix : </label><input type="text" name="prix" placeholder="Prix"></p>
             <label id="ajout_element">Marque : </label><input type="text" name="marque" placeholder="Marque"></p>
             <label id="ajout_element">Année de fabrication : </label><input type="text" name="anneefb" placeholder="Année Fabrication"></p>
             <label id="ajout_element">Date mise en service : </label><input type="text" name="datemes" placeholder="Date Mise en service"></p>
             <label id="ajout_element">Durée garantie : </label><input type="text" name="garantie" placeholder="Durée garantie"></p>
             <label id="ajout_element">Type : </label><input type="text" name="type" placeholder="Type">
+
             <div id ="msg">
-             <?php
+                </p><?php
                 $monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
                 if ($monUrl == "http://localhost/BDD/Site/ajout-element.php?erreur"){
                     echo ("Veuilliez saisir tous les champs ");
