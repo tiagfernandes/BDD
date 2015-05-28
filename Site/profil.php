edb6d89e1df2ede49f783a8ca6808b6f8624af4c
thomasplessiet <thomas.plessiet@laposte.net>
2015-05-28 15:20:26 +0200

    h

--- Site/profil.php
+++ Site/profil.php
@@ -8,7 +8,7 @@
 <meta charset="UTF-8">
 
    <head>
-    <title>Index</title>
+    <title>Profil</title>
     <link rel="shortcut icon" type="image/x-icon" href="./image/favicon.ico" />
     <link rel="icon" type="image/x-icon" href="./image/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="style.css">
