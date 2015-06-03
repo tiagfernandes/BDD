1c9a098bc453448161129d9beb05efa682be9d59
tiagfernandes <tiago_fernandes@live.fr>
2015-06-03 09:03:09 +0200

    ol

--- Site/ajout-document.php
+++ Site/ajout-document.php
@@ -24,9 +24,47 @@
                 <div id="form-ajout">
                     <fieldset><legend>Fiche document</legend>
 
-                        <form method="post" action="ajout.php">
+                        <form method="post" action="ajout_doc.php">
+                            <label id="ajout_element">Nom document : *</label><input type="text" name="nom_equipement" placeholder="Nom"></p>
+                            <label id="ajout_element">Etiquette document : *</label></p>
+                                <!-- 1ere listview -->
+                                <select name="">
+                                    <option value=NULL>-- Type --</option>
+                                    <?php
 
+                                    $reponse = $pdo->query('SELECT * FROM type_document');
+                                    while ($donnees = $reponse->fetch()){
+                                    ?>
+                                        <option value="<?php echo $donnees['idType_Document']; ?>"><?php echo $donnees['valeurTypeDoc']; ?> - <?php echo $donnees['typeDocument']; ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select> -
+                                <select name="">
+                                    <option value=NULL>-- Processus --</option>
+                                        <?php
 
+                                        $reponse = $pdo->query('SELECT * FROM processus');
+                                        while ($donnees = $reponse->fetch()){
+                                        ?>
+                                            <option value="<?php echo $donnees['idProcessus']; ?>"><?php echo $donnees['valeurProcessus']; ?> - <?php echo $donnees['Processus']; ?></option>
+                                        <?php
+                                        }
+                                        ?>
+                                    </option>
+                                </select> -
+                                <select name="">
+                                    <option value=NULL>-- Processus --</option>
+                                        <?php
+
+                                        $reponse = $pdo->query('SELECT * FROM processus');
+                                        while ($donnees = $reponse->fetch()){
+                                        ?>
+                                            <option value="<?php echo $donnees['idProcessus']; ?>"><?php echo $donnees['valeurProcessus']; ?> - <?php echo $donnees['Processus']; ?></option>
+                                        <?php
+                                        }
+                                        ?>
+                                    </option>
                         </form>
 
                     </fieldset>
