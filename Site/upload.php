8ed919bb9af32cc15353b754f840e9b8ae42a784
thomasplessiet <thomas.plessiet@laposte.net>
2015-06-09 09:15:49 +0200

    1

--- Site/upload.php
+++ Site/upload.php
@@ -1,16 +1,26 @@
 <?php
+    session_start ();
+
 if(isset($_FILES['avatar']))
 {
      $dossier = 'image/avatar/';
-     $fichier = basename($_FILES['avatar']['name']);
-     $image = 'image/avatar/'.$fichier.'';
-     if(move_uploaded_file($_FILES['avatar']['tmp_name'], $dossier . $fichier))
-     {
-          header('Location: profil.php');
-     }
-     else
-     {
-          header('Location: profil.php?erreur');
-     }
+     $filename = basename($_FILES['avatar']['name']);
+     $extension=strrchr($filename,'.');
+     $image = 'image/avatar/'.$filename.'';
+
+    if($extension!==FALSE){
+
+        if(move_uploaded_file($_FILES["avatar"]["tmp_name"], $dossier . $_SESSION["nom"] . $extension ))
+            {
+                header('Location: profil.php');
+            }
+        else
+            {
+                header('Location: profil.php?erreur');
+            }
+    }
+
+    else
+        header('Location: profil.php?erreur');
 }
 ?>
