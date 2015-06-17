9067071dcb17176675873e810f08ae29eb4d5a88
thomasplessiet <thomas.plessiet@laposte.net>
2015-06-17 09:40:36 +0200

    fiche de vie

---
+++ Site/fiche-vie.php
@@ -0,0 +1,23 @@
+<?php
+    require_once('fonctions.php');
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<meta charset="UTF-8">
+
+	<head>
+    	<title>Fiche de vie</title>
+			<link rel="shortcut icon" type="image/x-icon" href="./image/favicon.ico" />
+			<link rel="icon" type="image/x-icon" href="./image/favicon.ico" />
+			<link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+
+	<body>
+    	<?php require_once('entete.php'); ?>
+       	<div id="contenu">
+            <div id="banniere">Fiche de vie</div>
+    </body>
+</html>
